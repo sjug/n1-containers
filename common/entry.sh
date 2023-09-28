866e232e8971586520a30b4805b56fc02c4d0bb8
Sebastian Jug <seb@stianj.ug>
2023-09-28 10:45:29 -0400

    Have entrypoint move itself to a housekeeping core inside the pod

--- common/entry.sh
+++ common/entry.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 LD_PRELOAD=
-echo "Starting SSHD @ $(which sshd)"
-/sbin/sshd -D "$@"
+
+CUR_CPU_AFFINITY=$(taskset -cp $$ | awk -F': ' '{print $2}' )
+echo "Current shell CPU affinity is ${CUR_CPU_AFFINITY}" 
+FIRST_CPU_AFFINITY=$(echo ${CUR_CPU_AFFINITY} | awk -F '[^0-9]' '{print $1}')
+echo "Moving this process to CPU #${FIRST_CPU_AFFINITY}"
+taskset -cp "${FIRST_CPU_AFFINITY}" $$ 
+
+SSHD_LOCATION=$(which sshd)
+echo "Starting SSHD @ ${SSHD_LOCATION}"
+${SSHD_LOCATION} -D "$@"
