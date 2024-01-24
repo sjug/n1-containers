c2e19f7a8e536c4df60c36a8f176fb85589a5d34
Sebastian Jug <seb@stianj.ug>
2024-01-24 10:28:30 -0500

    Update setup for newer versions

--- common/setup.sh
+++ common/setup.sh
@@ -2,10 +2,14 @@
 #
 set -x
 
-SFNETTEST_LOCATION=https://github.com/Xilinx-CNS/cns-sfnettest/archive/refs/tags/sfnettest-1.6.0-rc1.tar.gz
+SFNETTEST_LOCATION=https://github.com/Xilinx-CNS/cns-sfnettest/archive/refs/tags/1.6.0-rc2.tar.gz
 SFNETTEST_NAME=cns-sfnettest
 SFNETTEST_ARCHIVE=${SFNETTEST_NAME}.tar.gz
 LINUXKI_LOCATION=https://github.com/HewlettPackard/LinuxKI/releases/download/7.7-1/linuxki-7.7-1.noarch.rpm
+ONLOAD_LOCATION=https://github.com/Xilinx-CNS/onload/archive/refs/tags/v8.1.2.tar.gz
+ONLOAD_NAME=onload
+ONLOAD_ARCHIVE=${ONLOAD_NAME}.tar.gz
+
 
 install_prereqs() {
   local EPEL_RPM=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
@@ -31,13 +35,13 @@ install_prereqs
 install_sysjitter
 
 # Get OpenOnload scripts
-curl -L https://github.com/Xilinx-CNS/onload/archive/refs/tags/v8.1.1.tar.gz -o onload.tar.gz
-tar xvf onload.tar.gz && rm onload.tar.gz
+curl -L ${ONLOAD_LOCATION} -o ${ONLOAD_ARCHIVE}
+tar xvf ${ONLOAD_ARCHIVE} && rm ${ONLOAD_ARCHIVE}
 cp onload-*/scripts/* /usr/local/bin
 
 # Get & build cns-sfnettest
 curl -L "${SFNETTEST_LOCATION}" -o "${SFNETTEST_ARCHIVE}"
-mkdir -p cns-sfnettest
+mkdir -p ${SNNETTEST_NAME}
 tar xzf "${SFNETTEST_ARCHIVE}" -C "${SFNETTEST_NAME}" --strip-components=1 && rm "${SFNETTEST_ARCHIVE}"
 cd "${SFNETTEST_NAME}"/src
 make
